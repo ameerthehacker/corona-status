aa7b5eeaa93fa9c8dc1e52cdd8bdaf2334bc5e23
Ameer Jhan <ameerjhanprof@gmail.com>
2020-03-17 17:21:13 +0530

    feat: show notification when new updates are available

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import useToast from '@chakra-ui/core/dist/Toast';
+import Box from '@chakra-ui/core/dist/Box';
+import Text from '@chakra-ui/core/dist/Text';
+import Link from '@chakra-ui/core/dist/Link';
 import * as serviceWorker from './serviceWorker';
 import App from './modules/app/app';
 import APIServiceContext from './contexts/api';
@@ -9,6 +13,8 @@ import ThemeProvider from '@chakra-ui/core/dist/ThemeProvider';
 import CSSReset from '@chakra-ui/core/dist/CSSReset';
 import ColorModeProvider from '@chakra-ui/core/dist/ColorModeProvider';
 import { HistoryService } from './services/history/history';
+import Icon from '@chakra-ui/core/dist/Icon';
+import Stack from '@chakra-ui/core/dist/Stack';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -19,22 +25,57 @@ if (!API_URL) {
 const httpService = new APIService(API_URL);
 const historyService = new HistoryService();
 
+function Root() {
+  const toast = useToast();
+
+  useEffect(() => {
+    serviceWorker.register({
+      onUpdate() {
+        toast({
+          render: function ToastBody() {
+            return (
+              <Box
+                m={3}
+                color="white"
+                p={3}
+                borderRadius={5}
+                boxShadow="lg"
+                bg="blue.500"
+              >
+                <Stack direction="row" alignItems="center">
+                  <Icon name="info" />
+                  <Text>Woah! new updates are available</Text>
+                  <Link
+                    fontWeight="bold"
+                    onClick={() => window.location.reload()}
+                  >
+                    REFRESH
+                  </Link>
+                </Stack>
+              </Box>
+            );
+          }
+        });
+      }
+    });
+  }, [toast]);
+
+  return (
+    <APIServiceContext.Provider value={httpService}>
+      <HistoryServiceContext.Provider value={historyService}>
+        <App />
+      </HistoryServiceContext.Provider>
+    </APIServiceContext.Provider>
+  );
+}
+
 const el = (
-  <APIServiceContext.Provider value={httpService}>
-    <HistoryServiceContext.Provider value={historyService}>
-      <ThemeProvider>
-        <CSSReset />
-        <ColorModeProvider>
-          <App />
-        </ColorModeProvider>
-      </ThemeProvider>
-    </HistoryServiceContext.Provider>
-  </APIServiceContext.Provider>
+  <ThemeProvider>
+    <CSSReset />
+    <ColorModeProvider>
+      <Root />
+    </ColorModeProvider>
+  </ThemeProvider>
 );
 
 ReactDOM.render(el, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
